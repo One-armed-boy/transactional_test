c6ba6ff6ce5d553daef7e49bde78b2ad7604ad0f
One-armed-boy <fanatic5500@gmail.com>
2024-02-04 03:46:02 +0900

    비관적 락을 위한 업데이트 전용 유저 조회 메서드 생성

--- src/main/java/org/example/UserRepository.java
+++ src/main/java/org/example/UserRepository.java
@@ -1,8 +1,16 @@
 package org.example;
 
+import jakarta.persistence.LockModeType;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, String> {
+  @Transactional
+  @Lock(value = LockModeType.PESSIMISTIC_WRITE)
+  Optional<User> findForUpdateByUser(String id);
 }
